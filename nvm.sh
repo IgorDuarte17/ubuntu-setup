c26c72fd490dea1aba3c7c60bede3d27e6ec986a
Igor Santos <igors.d@hotmail.com>
2020-06-08 20:51:53 -0300

    Add nvm script

---
+++ nvm.sh
@@ -0,0 +1,8 @@
+# Install NVM
+
+echo 'installing NVM' 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Export NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
