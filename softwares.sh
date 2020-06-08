da68e5d48cd84db989e1760c847de546ef9a579f
Igor Santos <igors.d@hotmail.com>
2020-06-08 20:49:50 -0300

    Add script para softwares

---
+++ softwares.sh
@@ -0,0 +1,40 @@
+# Install Softwares
+
+
+# Install Spotify
+echo 'installing Spotify' 
+snap install spotify
+clear
+
+
+# Install Postman
+echo 'installing Postman' 
+snap install postman
+clear
+
+
+# Install Chrome
+echo 'installing Chrome' 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+clear
+
+
+# Install Slack
+echo 'installing Slack' 
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
+sudo apt install ./slack-desktop-*.deb -y
+clear
+
+
+# Install Discord
+echo 'installing Discord' 
+wget -O discord-0.0.1.deb https://discordapp.com/api/download?platform=linux&format=deb
+sudo dpkg -i discord-0.0.1.deb
+clear
+
+
+# Install vim
+echo 'installing vim'
+sudo apt install vim -y
+clear
