2214ee57906302f5390dca54e4f061f2ca506672
Igor Santos <igors.d@hotmail.com>
2020-06-08 20:51:22 -0300

    Add docker script

---
+++ docker.sh
@@ -0,0 +1,51 @@
+# Install Dependencies
+echo 'installing Dependencies' 
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+    
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+    
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+
+   
+# Install Docker Engine
+echo 'installing Docker Engine'
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
+# Permissions
+echo 'Configure Permissions'
+echo "What your user.name?"
+read user_name
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG #Check groups
+sudo usermod -aG docker $user_name
+
+
+# Install Docker Compose
+echo 'installing Docker Compose'
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Permissions
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+# Check Version
+echo 'Check Versions'
+docker --version
+docker-compose --version
