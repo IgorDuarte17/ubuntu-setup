63086062520f40ee7936ec7cbf43fd6fe766362d
Igor Santos <igors.d@hotmail.com>
2020-06-08 20:51:06 -0300

    Add git script

---
+++ git.sh
@@ -0,0 +1,25 @@
+# Install Git
+
+echo 'installing git' 
+sudo apt install git -y
+
+
+echo "What name do you want to use in GIT user.name?"
+echo "For example, mine will be \"Igor Santos\""
+read git_config_user_name
+git config --global user.name "$git_config_user_name"
+clear 
+
+
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"igors.d@hotmail.com"
+read git_config_user_email
+git config --global user.email $git_config_user_email
+clear
+
+
+echo "Generating a SSH Key"
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
