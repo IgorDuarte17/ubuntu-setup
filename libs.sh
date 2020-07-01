2b7253cc269ce254f51f05130db0891f404f042b
Igor Santos <igors.d@hotmail.com>
2020-07-01 20:15:05 -0300

    Create libs.sh

---
+++ libs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install Libs
+
+
+# Install vim
+echo 'installing vim'
+sudo apt install vim -y
+clear
+
+
+#Install Make
+echo 'installing Make'
+sudo apt-get install make
+clear
+
+
+#Install Net-Tools
+echo 'installing net-tools'
+sudo apt install net-tools
+clear
