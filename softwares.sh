9b4ebf2005b7c72ca83a663afb429eb301798f21
Igor Santos <igors.d@hotmail.com>
2020-07-01 20:23:57 -0300

    Update softwares.sh

--- softwares.sh
+++ softwares.sh
@@ -15,13 +15,6 @@ snap install postman
 clear
 
 
-# Install Chrome
-echo 'installing Chrome' 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-clear
-
-
 # Install Slack
 echo 'installing Slack' 
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
