ba6376e0a79a5ac49a6891f1315295d1d5b8839a
Igor Santos <igors.d@hotmail.com>
2020-07-01 20:34:42 -0300

    Create chrome.sh

---
+++ chrome.sh
@@ -0,0 +1,20 @@
+ 
+#!/bin/bash
+
+# Install Chrome
+echo 'installing Chrome' 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+clear
+
+# Install Extensions
+echo 'installing extensions'
+
+install_chrome_extension "gighmmpiobklfepjocnamgkkbiglidom" "AdBlock"
+install_chrome_extension "jlmpjdjjbgclbocgajdjefcidcncaied" "Daily 2.0"
+install_chrome_extension "bhlhnicpbhignbdhedgjhgdocnmhomnp" "ColorZilla"
+
+# Developer Extensions
+install_chrome_extension "odkdlljoangmamjilkamahebpkgpeacp" "GitHub Dark Theme"
+install_chrome_extension "bcjindcccaagfpapjjmafapmmgkkhgoa" "JSON Formatter"
+install_chrome_extension "nhdogjmejiglipccpnnnanhbledajbpd" "Vue.js devtools"
