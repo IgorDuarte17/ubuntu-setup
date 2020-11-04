ea27d800e8c628dd2c53051db7341b0b9ee779e2
Igor Santos <igors.d@hotmail.com>
2020-11-03 23:43:10 -0300

    Update vs-code.sh

--- development/vs-code.sh
+++ development/vs-code.sh
@@ -14,32 +14,38 @@ sudo apt-get install code -y
 # Install Extensions
 echo 'installing extensions'
 
+# PHP
 code --install-extension felixfbecker.php-debug
 code --install-extension felixfbecker.php-intellisense
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension bmewburn.vscode-intelephense-client
 code --install-extension mehedidracula.php-namespace-resolver
+
+# Laravel
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
 code --install-extension onecentlin.laravel-blade
 code --install-extension onecentlin.laravel5-snippets
 code --install-extension ryannaddy.laravel-artisan
 code --install-extension codingyu.laravel-goto-view
+code --install-extension stef-k.laravel-goto-controller
 
+# Tools
 code --install-extension mikestead.dotenv
 code --install-extension editorconfig.editorconfig
 code --install-extension foxundermoon.shell-format
 code --install-extension yzhang.markdown-all-in-one
-
 code --install-extension eamodio.gitlens
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension humao.rest-client
-
 code --install-extension alefragnani.bookmarks
 code --install-extension gruntfuggly.todo-tree
 code --install-extension wayou.vscode-todo-highlight
+code --install-extension coenraads.bracket-pair-colorizer-2
 
+# Front e JS
 code --install-extension formulahendry.auto-close-tag
 code --install-extension christian-kohler.path-intellisense
-code --install-extension coenraads.bracket-pair-colorizer-2
 code --install-extension naumovs.color-highlight
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension xabikos.javascriptsnippets
 code --install-extension octref.vetur
-
