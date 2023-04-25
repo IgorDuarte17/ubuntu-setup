86475275031544845abf0a9e8bb12001078908a7
Igor Duarte <igors.d@hotmail.com>
2023-04-25 20:46:03 -0300

    Update mysql-workbench.sh

--- database/mysql-workbench.sh
+++ database/mysql-workbench.sh
@@ -5,3 +5,7 @@
 echo 'installing MySQL Workbench' 
 sudo snap install mysql-workbench-community
 clear
+
+echo 'installing Password Manager Service'
+sudo snap connect mysql-workbench-community:password-manager-service :password-manager-service
+clear
