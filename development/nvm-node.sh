b8ca52f48334ca05f4a60836610d323de7bb056e
Igor Duarte <igors.d@hotmail.com>
2023-04-25 21:03:02 -0300

    Update and rename nvm.sh to nvm-node.sh

--- development/nvm-node.sh
+++ development/nvm-node.sh
@@ -8,3 +8,21 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 # Export NVM
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Check Version
+nvm --version
+clear
+
+
+# Install Node
+echo 'installing Node' 
+nvm install node
+
+echo 'install Specific Node Version'
+nvm install $version #Exe: 14.20.0
+
+echo 'use Specific Node Version'
+nvm use $version #Exe: 14.20.0
+
+# Check Version
+node --version
