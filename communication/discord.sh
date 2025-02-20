e5811fb33a0cdff0ce79fb47cf315f250043930b
Igor Duarte <igors.d@hotmail.com>
2025-02-20 14:01:59 -0300

    Update discord.sh

--- communication/discord.sh
+++ communication/discord.sh
@@ -2,6 +2,6 @@
 
 # Install Discord
 echo 'installing Discord' 
-wget -O discord-0.0.1.deb https://discordapp.com/api/download?platform=linux&format=deb
+wget -O discord.deb "https://discord.com/api/download?platform=linux&format=deb"
 sudo dpkg -i discord-0.0.1.deb
 clear
